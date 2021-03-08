63be7ddef97a807f7ef3bc17531992d8bfcfa202
dingnan <nandingb@gmail.com>
2021-03-08 18:57:01 -0500

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentAComponent } from './components/component-a/component-a.component';
+import { ComponentBComponent } from './components/component-b/component-b.component';
+import { ComponentCComponent } from './components/component-c/component-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentAComponent,
+    ComponentBComponent,
+    ComponentCComponent
   ],
   imports: [
     BrowserModule,
