3ebeb5f17c3a5fb17e17cea12b3466aa0adf7b2e
dingnan <nandingb@gmail.com>
2021-03-09 06:17:32 -0500

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentAComponent } from './components/component-a/component-a.component';
+import { ComponentBComponent } from './components/component-b/component-b.component';
+import { ComponentCComponent } from './components/component-c/component-c.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'a', component: ComponentAComponent},
+  {path:'b', component: ComponentBComponent},
+  {path:'c', component: ComponentCComponent},
+  {path:'**', component: PageNotFoundComponent}, // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ComponentAComponent } from './components/component-a/component-a.component';
 import { ComponentBComponent } from './components/component-b/component-b.component';
 import { ComponentCComponent } from './components/component-c/component-c.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentAComponent,
     ComponentBComponent,
-    ComponentCComponent
+    ComponentCComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
